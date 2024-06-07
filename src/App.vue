2e4f93e96cb07fec7b06f259f4545006d69405e4
carlosismaelad <carlosismaelad@outlook.com>
2024-06-07 00:03:31 -0300

    Initial commit - Creating Banner component

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: {Banner}
+}
+</script>
+
+Script
+
+<template>
+  <Banner/>
+</template>
+
+<style scoped>
+
+</style>
