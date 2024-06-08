2438e16ad729f72b3804463d7970ae77fff0e135
carlosismaelad <carlosismaelad@outlook.com>
2024-06-08 13:48:27 -0300

    Rendering categories from a fetch to a Github gist

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,11 +1,15 @@
 <script lang="ts">
-import obterCategorias from '@/http/index';
+import { obterCategorias } from '@/http/index';
+import type ICategoria from '@/interfaces/ICategoria';
 
 export default {
     data() {
         return{
-            categorias: obterCategorias()
+            categorias: [] as ICategoria[]
         }
+    },
+    async created(){
+      this.categorias = await obterCategorias()
     }
 }
 </script>
